b4a168f17e76f5bb6be13b516666fb3a912f4661
Korbinian Koch <korbinian-koch@web.de>
2023-05-16 12:41:12 +0200

    adjust histogram ticks

--- src/match.py
+++ src/match.py
@@ -52,7 +52,7 @@ for i in range(n):
 # plot the histograms in terminal
 plotext.subplots(1, len(best_corrs))
 thresholds = np.linspace(-1, 1, 41)
-labels = [f">{x:.1f}" for x in thresholds[:-1]]
+labels = [f">{x:.2f}" for x in thresholds[:-1]]
 for i, best_corr in enumerate(best_corrs):
     plotext.subplot(1, i + 1).plotsize(plotext.tw() // len(best_corrs), None)
     plotext.subplot(1, i + 1).title(f"Conv2d #{i+1}")
