bbf36e372bfa80a82dbe9ff0532757ff20a901bc
Korbinian Koch <korbinian-koch@web.de>
2023-05-26 16:29:20 +0200

    change figure

--- src/plot_scripts/plot_ranked_filters.py
+++ src/plot_scripts/plot_ranked_filters.py
@@ -8,7 +8,7 @@ from src.models.VGG import VGG
 from src.utils.utils import load_model, normalize, get_plots_dir
 
 
-TITLE = "CIFAR10, VGG11"
+TITLE = "CIFAR10, VGG11, 1× width"
 MODEL_TO_PLOT = "VGG11-1x-a"
 model = VGG(11)
 
