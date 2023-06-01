5969f968029845c53a50d0cbf927df30dcf7eb4f
Korbinian Koch <korbinian-koch@web.de>
2023-06-01 16:57:23 +0200

    edit codecarbon decorator

--- src/train.py
+++ src/train.py
@@ -15,7 +15,7 @@ from rich import pretty
 pretty.install()
 
 
-@track_emissions()
+@track_emissions(log_level="error")
 def main():
     train_aug_loader, _, test_loader = get_loaders(args.dataset)
     model = VGG(size=args.size, width=args.width).cuda()
