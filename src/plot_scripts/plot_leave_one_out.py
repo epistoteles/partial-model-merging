b49fe13b02b936aef568d7872166d3c6fefb0a4f
Korbinian Koch <korbinian-koch@web.de>
2023-10-16 18:21:17 +0200

    fix experiment_a error

--- src/plot_scripts/plot_leave_one_out.py
+++ src/plot_scripts/plot_leave_one_out.py
@@ -14,16 +14,15 @@ def plot_leave_one_out(model_name_a: str, model_name_b: str):
     dataset_a, model_type_a, size_a, batch_norm_a, width_a, variant_a = parse_model_name(model_name_a)
     dataset_b, model_type_b, size_b, batch_norm_b, width_b, variant_b = parse_model_name(model_name_b)
 
+    metrics_a = evaluate_single_model(model_name_a)
+    metrics_b = evaluate_single_model(model_name_b)
     metrics = experiment_a(model_name_a, model_name_b)
 
-    for i, (metric, split) in enumerate(product(["accs", "losses"], ["train", "test"])):
+    for i, (split, metric) in enumerate(product(["train", "test"], ["accs", "losses"])):
         plt.figure(figsize=(12, 8))
         plt.xlabel("layer")
         plt.ylabel(f"{split} {metric}")
         plt.title(f"{dataset_a}, {model_type_a}{size_a}, {width_a}×width, leave-one-out experiment")
-
-        metrics_a = evaluate_single_model(model_name_a)
-        metrics_b = evaluate_single_model(model_name_b)
 
         sns.lineplot(
             x=metrics["layers"], y=metrics[f"full_ensembling_{split}_{metric}"], label="full ensembling", color="blue"
