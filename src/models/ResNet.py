184ef1551f6d9eb91c1f817f1290a7205ea3c94c
Korbinian Koch <korbinian-koch@web.de>
2023-11-13 21:39:06 +0100

    fix zip iteration error

--- src/models/ResNet.py
+++ src/models/ResNet.py
@@ -90,7 +90,7 @@ class ResNet18(nn.Module):
         assert planes[1] == planes[3]
         strides = [stride] + [1] * (num_blocks - 1)
         layers = []
-        for p1, p2, stride in zip(zip(planes[::2], planes[1::2]), strides):
+        for (p1, p2), stride in zip(zip(planes[::2], planes[1::2]), strides):
             layers.append(block(in_planes=self.in_planes, mid_planes=p1, out_planes=p2, stride=stride))
             self.in_planes = p2
         return nn.Sequential(*layers)
