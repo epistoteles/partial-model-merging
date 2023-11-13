177b800baf54d0c86bc9bba96474b9874200311c
Korbinian Koch <korbinian-koch@web.de>
2023-11-13 18:04:52 +0100

    add ResNet is_buffer fix

--- src/dev_scripts/add_buffer_flag.py
+++ src/dev_scripts/add_buffer_flag.py
@@ -1,4 +1,4 @@
-# This script adds the is_buffer flag to old .safetensors checkpoints.
+# This script adds the is_buffer flag to old .safetensors checkpoints that don't have it.
 # If you are using the most current version of this repo you will never need it.
 
 import torch
@@ -10,7 +10,7 @@ from collections import defaultdict
 
 def _sd_item_to_key(item):
     _key = item[0].split(".")
-    _key[1] = _convert(_key[1])
+    _key = [_convert(x) for x in _key]
     return _key
 
 
@@ -24,8 +24,36 @@ def _convert(x):
 ####################
 # fix VGGs with bn #
 ####################
+#
+# model_names = [name for name in get_all_model_names() if "VGG" in name and "bn" in name]
+#
+# for filename in model_names:
+#     filename += ".safetensors"
+#     checkpoints_dir = _get_checkpoints_dir()
+#     filename = os.path.join(checkpoints_dir, filename)
+#
+#     state_dict = load_file(filename)
+#
+#     d = defaultdict(list)
+#     entries = list(map(lambda x: x.split("."), state_dict.keys()))
+#     for entry in entries:
+#         if len(entry) == 3:
+#             d[entry[1]].append(entry[2])
+#
+#     for key, values in d.items():
+#         if "is_buffer" not in values:
+#             state_dict[f"features.{key}.is_buffer"] = torch.zeros_like(state_dict[f"features.{key}.bias"])
+#
+#     state_dict = dict(sorted(state_dict.items(), key=_sd_item_to_key))
+#
+#     save_file(state_dict, filename)
 
-model_names = [name for name in get_all_model_names() if "VGG" in name and "bn" in name]
+
+###############
+# fix ResNets #
+###############
+
+model_names = [name for name in get_all_model_names() if "ResNet" in name and "bn" in name]
 
 for filename in model_names:
     filename += ".safetensors"
@@ -34,15 +62,13 @@ for filename in model_names:
 
     state_dict = load_file(filename)
 
-    d = defaultdict(list)
-    entries = list(map(lambda x: x.split("."), state_dict.keys()))
-    for entry in entries:
-        if len(entry) == 3:
-            d[entry[1]].append(entry[2])
-
-    for key, values in d.items():
-        if "is_buffer" not in values:
-            state_dict[f"features.{key}.is_buffer"] = torch.zeros_like(state_dict[f"features.{key}.bias"])
+    for key in list(state_dict.keys()):
+        if key.endswith("num_batches_tracked"):
+            continue
+        buffer_name = ".".join([key.rsplit(".", 1)[0], "is_buffer"])
+        if buffer_name not in state_dict.keys():
+            dim = state_dict[key].shape[0]
+            state_dict[buffer_name] = torch.zeros(dim).bool()
 
     state_dict = dict(sorted(state_dict.items(), key=_sd_item_to_key))
 
--- src/models/ResNet.py
+++ src/models/ResNet.py
@@ -17,62 +17,27 @@ class BasicBlock(nn.Module):
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv1.is_buffer = nn.Parameter(torch.zeros(planes).bool(), requires_grad=False)
         self.bn1 = nn.BatchNorm2d(planes)
+        self.bn1.is_buffer = nn.Parameter(torch.zeros(planes).bool(), requires_grad=False)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv2.is_buffer = nn.Parameter(torch.zeros(planes).bool(), requires_grad=False)
         self.bn2 = nn.BatchNorm2d(planes)
+        self.bn2.is_buffer = nn.Parameter(torch.zeros(planes).bool(), requires_grad=False)
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False),
                 nn.BatchNorm2d(planes),
             )
+            self.shortcut[0].is_buffer = nn.Parameter(torch.zeros(planes).bool(), requires_grad=False)
+            self.shortcut[1].is_buffer = nn.Parameter(torch.zeros(planes).bool(), requires_grad=False)
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
         out += self.shortcut(x)
         out = F.relu(out)
-        return out
-
-
-class ResNet20(nn.Module):
-    def __init__(self, width: float | list[float] | torch.FloatTensor = 1.0, num_classes: int = 10):
-        """
-        A custom ResNet18 module, adapted from https://github.com/KellerJordan/REPAIR
-        :param width: multiplier for the width of the network
-        :param num_classes: the number of output classes
-        """
-        super().__init__()
-        self.size = 18
-        self.bn = True
-        self.num_classes = num_classes
-        self.in_planes = round(width * 16)
-
-        self.conv1 = nn.Conv2d(3, round(width * 16), kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(round(width * 16))
-        self.layer1 = self._make_layer(BasicBlock, planes=round(width * 16), num_blocks=3, stride=1)
-        self.layer2 = self._make_layer(BasicBlock, planes=round(width * 32), num_blocks=3, stride=2)
-        self.layer3 = self._make_layer(BasicBlock, planes=round(width * 64), num_blocks=3, stride=2)
-        self.linear = nn.Linear(round(width * 64), num_classes)
-
-        self.apply(_weights_init)
-
-    def _make_layer(self, block, planes: int, num_blocks: int, stride: int):
-        strides = [stride] + [1] * (num_blocks - 1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = F.avg_pool2d(out, out.size()[3])
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
         return out
 
 
@@ -84,14 +49,15 @@ class ResNet18(nn.Module):
         :param num_classes: the number of output classes
         """
         super().__init__()
-        self.size = 20
+        self.size = 18
         self.bn = True
         self.num_classes = num_classes
         self.in_planes = round(width * 16)
 
         self.conv1 = nn.Conv2d(3, round(width * 16), kernel_size=3, stride=1, padding=1, bias=False)
-        self.conv1.is_buffer = nn.Parameter(torch.zeros(round(width * 16)), requires_grad=False)
+        self.conv1.is_buffer = nn.Parameter(torch.zeros(round(width * 16)).bool(), requires_grad=False)
         self.bn1 = nn.BatchNorm2d(round(width * 16))
+        self.bn1.is_buffer = nn.Parameter(torch.zeros(round(width * 16)).bool(), requires_grad=False)
         self.layer1 = self._make_layer(BasicBlock, planes=round(width * 16), num_blocks=2, stride=1)
         self.layer2 = self._make_layer(BasicBlock, planes=round(width * 32), num_blocks=2, stride=2)
         self.layer3 = self._make_layer(BasicBlock, planes=round(width * 64), num_blocks=2, stride=2)
@@ -114,6 +80,49 @@ class ResNet18(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
+        out = F.avg_pool2d(out, out.size()[3])
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+class ResNet20(nn.Module):
+    def __init__(self, width: float | list[float] | torch.FloatTensor = 1.0, num_classes: int = 10):
+        """
+        A custom ResNet18 module, adapted from https://github.com/KellerJordan/REPAIR
+        :param width: multiplier for the width of the network
+        :param num_classes: the number of output classes
+        """
+        super().__init__()
+        self.size = 20
+        self.bn = True
+        self.num_classes = num_classes
+        self.in_planes = round(width * 16)
+
+        self.conv1 = nn.Conv2d(3, round(width * 16), kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv1.is_buffer = nn.Parameter(torch.zeros(round(width * 16)).bool(), requires_grad=False)
+        self.bn1 = nn.BatchNorm2d(round(width * 16))
+        self.bn1.is_buffer = nn.Parameter(torch.zeros(round(width * 16)).bool(), requires_grad=False)
+        self.layer1 = self._make_layer(BasicBlock, planes=round(width * 16), num_blocks=3, stride=1)
+        self.layer2 = self._make_layer(BasicBlock, planes=round(width * 32), num_blocks=3, stride=2)
+        self.layer3 = self._make_layer(BasicBlock, planes=round(width * 64), num_blocks=3, stride=2)
+        self.linear = nn.Linear(round(width * 64), num_classes)
+
+        self.apply(_weights_init)
+
+    def _make_layer(self, block, planes: int, num_blocks: int, stride: int):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
         out = F.avg_pool2d(out, out.size()[3])
         out = out.view(out.size(0), -1)
         out = self.linear(out)
