aafe7730da6f3f42b9d5f7145a332d5e1fcade1c
Korbinian Koch <korbinian-koch@web.de>
2023-11-20 13:34:38 +0100

    only permute inter layer 5

--- src/utils.py
+++ src/utils.py
@@ -531,21 +531,21 @@ def permute_model(reference_model: torch.nn.Module, model: torch.nn.Module, load
             permute_output(perm_map, subnet_model[-1].conv1, subnet_model[-1].bn1)
             permute_input(perm_map, subnet_model[-1].conv2)
         # inter-block permutation
-        # for layer in [5, 9, 13, 17]:
-        #     subnet_ref = subnet(reference_model, layer)
-        #     subnet_model = subnet(model, layer)
-        #     if layer >= 9:
-        #         permute_input(perm_map, [subnet_model[-2].conv1, subnet_model[-2].downsample[0]])
-        #     perm_map = get_layer_perm(subnet_ref, subnet_model, loader)
-        #     if layer == 5:  # special case for first conv
-        #         permute_output(perm_map, model.conv1, model.bn1)
-        #         permute_input(perm_map, [subnet_model[-1].conv1, subnet_model[-2].conv1])
-        #     else:
-        #         permute_input(perm_map, [subnet_model[-1].conv1])
-        #     if layer == 17:
-        #         permute_input(perm_map, model.linear)
-        #     permute_output(perm_map, subnet_model[-1].conv2, subnet_model[-1].bn2)
-        #     permute_output(perm_map, subnet_model[-2].conv2, subnet_model[-2].bn2)
+        for layer in [5]:  # , 9, 13, 17]:
+            subnet_ref = subnet(reference_model, layer)
+            subnet_model = subnet(model, layer)
+            if layer >= 9:
+                permute_input(perm_map, [subnet_model[-2].conv1, subnet_model[-2].downsample[0]])
+            perm_map = get_layer_perm(subnet_ref, subnet_model, loader)
+            if layer == 5:  # special case for first conv
+                permute_output(perm_map, model.conv1, model.bn1)
+                permute_input(perm_map, [subnet_model[-1].conv1, subnet_model[-2].conv1])
+            else:
+                permute_input(perm_map, [subnet_model[-1].conv1])
+            if layer == 17:
+                permute_input(perm_map, model.linear)
+            permute_output(perm_map, subnet_model[-1].conv2, subnet_model[-1].bn2)
+            permute_output(perm_map, subnet_model[-2].conv2, subnet_model[-2].bn2)
 
     else:
         raise ValueError(f"Unknown model type {type(model)}")
