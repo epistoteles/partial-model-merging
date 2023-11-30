eaea0318398ae21bc4c135a3dc58208887fd3dec
Korbinian Koch <korbinian-koch@web.de>
2023-11-30 18:44:05 +0100

    update eval scripts

--- eval_mlps_bn.py
+++ eval_mlps_bn.py
@@ -1,10 +1,11 @@
+from src.utils import get_all_model_names
 from src.evaluate import evaluate_two_models
 from src.plot_scripts.plot_acc_and_loss_curves import plot_acc_and_loss_curves
 from src.plot_scripts.plot_correlation_histogram import plot_correlation_histogram
 
-for w in [0.125, 0.25, 0.5, 1.0, 2.0]:  # 4, 8
-    for s in range(3, 11):
-        model_name = f"MNIST-MLP{s}-bn-{w}x"
-        evaluate_two_models(model_name, expansions=[1.1, 1.5])
-        plot_acc_and_loss_curves(model_name)
-        plot_correlation_histogram(model_name)
+model_stems = {x[:-2] for x in get_all_model_names() if "MLP" in x and "bn" in x}
+
+for model_name in model_stems:
+    evaluate_two_models(model_name, expansions=[1.1, 1.5])
+    plot_acc_and_loss_curves(model_name)
+    plot_correlation_histogram(model_name)
