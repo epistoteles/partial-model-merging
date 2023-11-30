82b20f8f1cf75bd1dfb3039b8533b59fa52c03dc
Korbinian Koch <korbinian-koch@web.de>
2023-12-01 00:33:59 +0100

    update eval scripts

--- eval_mlps_bn.py
+++ eval_mlps_bn.py
@@ -3,9 +3,8 @@ from src.evaluate import evaluate_two_models
 from src.plot_scripts.plot_acc_and_loss_curves import plot_acc_and_loss_curves
 from src.plot_scripts.plot_correlation_histogram import plot_correlation_histogram
 
-model_stems = {x[:-2] for x in get_all_model_names() if "MLP" in x and "bn" in x}
-
-for model_name in model_stems:
-    evaluate_two_models(model_name, expansions=[1.1, 1.5])
+for w in [0.125, 0.25, 0.5, 1, 2, 4, 8]:
+    model_name = f"MNIST-MLP5-bn-{w}x"
+    evaluate_two_models(model_name, expansions=[1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0])
     plot_acc_and_loss_curves(model_name)
     plot_correlation_histogram(model_name)
