0fd68b6c1ca11ac8ce01e9893a9ea721c0189947
Korbinian Koch <korbinian-koch@web.de>
2023-12-03 17:25:16 +0100

    start plot_grid

--- src/plot_scripts/plot_grids.py
+++ src/plot_scripts/plot_grids.py
@@ -6,7 +6,8 @@ from itertools import product
 
 import torch
 
-from src.utils import load_model, normalize, get_plots_dir, parse_model_name, get_all_model_names
+from safetensors.torch import load_file
+from src.utils import load_model, normalize, get_plots_dir, parse_model_name, get_all_model_names, get_evaluations_dir
 from src.evaluate import evaluate_two_models
 
 
@@ -16,7 +17,8 @@ def plot_reduction(dataset: str, architecture: str, bn: bool):
     endpoint_accuracies = torch.zeros(len(widths), len(sizes))
     for i, size in enumerate(sizes):
         for j, width in enumerate(widths):
-            metrics = evaluate_two_models(f"{dataset}-{architecture}{size}-{'bn-' if bn else ''}{width}x")
+            dir = get_evaluations_dir(subdir="two_models")
+            metrics = load_file(f"{dir}/{dataset}-{architecture}{size}-{'bn-' if bn else ''}{width}x-ab.safetensors")
             acc_a = metrics["merging_test_accs"][0].item()
             acc_b = metrics["merging_test_accs"][-1].item()
             acc_avg = (acc_a + acc_b) / 2
