b25fcd1643a558249fe5f3c9f77a544e807487ad
Korbinian Koch <korbinian-koch@web.de>
2023-12-04 13:38:35 +0100

    add more widths to plot

--- src/plot_scripts/plot_AUC_VGG.py
+++ src/plot_scripts/plot_AUC_VGG.py
@@ -14,13 +14,14 @@ dataset = "CIFAR10"
 architecture = "VGG"
 bn = True
 
+metrics = ["acc", "loss"]
 sizes = [11, 13, 16, 19]
 widths = [0.25, 0.5, 1, 2]
 expansions = [1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0]
-accs_endpoint = torch.zeros(len(widths), len(sizes))
-accs_ensembling = torch.zeros(len(widths), len(sizes))
-accs_partial_merging = torch.zeros(len(widths), len(sizes), len(expansions))
-accs_partial_merging_REPAIR = torch.zeros(len(widths), len(sizes), len(expansions))
+endpoint = torch.zeros(len(metrics), len(widths), len(sizes))
+ensembling = torch.zeros(len(metrics), len(widths), len(sizes))
+partial_merging = torch.zeros(len(metrics), len(widths), len(sizes), len(expansions))
+partial_merging_REPAIR = torch.zeros(len(metrics), len(widths), len(sizes), len(expansions))
 
 for i, size in enumerate(sizes):
     for j, width in enumerate(widths):
@@ -33,167 +34,89 @@ for i, size in enumerate(sizes):
             acc_avg = (acc_a + acc_b) / 2
             acc_ensembling = metrics["ensembling_test_accs"][10].item()
 
-            accs_endpoint[j, i] = acc_avg
-            accs_ensembling[j, i] = acc_ensembling
+            loss_a = metrics["ensembling_test_losses"][0].item()
+            loss_b = metrics["ensembling_test_losses"][-1].item()
+            loss_avg = (loss_a + loss_b) / 2
+            loss_ensembling = metrics["ensembling_test_losses"][10].item()
 
-            accs_partial_merging[j, i, 0] = metrics["merging_test_accs"][10]
-            accs_partial_merging_REPAIR[j, i, 0] = metrics["merging_REPAIR_test_accs"][10]
+            endpoint[0, j, i] = acc_avg
+            ensembling[0, j, i] = acc_ensembling
+            endpoint[1, j, i] = loss_avg
+            ensembling[1, j, i] = loss_ensembling
+
+            partial_merging[0, j, i, 0] = metrics["merging_test_accs"][10]
+            partial_merging_REPAIR[0, j, i, 0] = metrics["merging_REPAIR_test_accs"][10]
+            partial_merging[1, j, i, 0] = metrics["merging_test_losses"][10]
+            partial_merging_REPAIR[1, j, i, 0] = metrics["merging_REPAIR_test_losses"][10]
 
             for idx, k in enumerate(expansions[1:]):
-                accs_partial_merging[j, i, idx + 1] = metrics[f"partial_merging_{k}_test_accs"][10]
-                accs_partial_merging_REPAIR[j, i, idx + 1] = metrics[f"partial_merging_REPAIR_{k}_test_accs"][10]
+                partial_merging[0, j, i, idx + 1] = metrics[f"partial_merging_{k}_test_accs"][10]
+                partial_merging_REPAIR[0, j, i, idx + 1] = metrics[f"partial_merging_REPAIR_{k}_test_accs"][10]
+                partial_merging[1, j, i, idx + 1] = metrics[f"partial_merging_{k}_test_losses"][10]
+                partial_merging_REPAIR[1, j, i, idx + 1] = metrics[f"partial_merging_REPAIR_{k}_test_losses"][10]
 
-full_barrier_absolute = accs_endpoint.unsqueeze(-1) - accs_partial_merging[:, :, 0:1]
-barrier_reduction_absolute = accs_partial_merging - accs_partial_merging[:, :, 0:1]
+full_barrier_absolute = endpoint.unsqueeze(-1) - partial_merging[:, :, :, 0:1]
+barrier_reduction_absolute = partial_merging - partial_merging[:, :, :, 0:1]
 barrier_reduction_relative = barrier_reduction_absolute / full_barrier_absolute
 
-barrier_reduction_absolute_REPAIR = accs_partial_merging_REPAIR - accs_partial_merging[:, :, 0:1]
+barrier_reduction_absolute_REPAIR = partial_merging_REPAIR - partial_merging[:, :, :, 0:1]
 barrier_reduction_relative_REPAIR = barrier_reduction_absolute_REPAIR / full_barrier_absolute
 
 
-######################
-# plot w.r.t. buffer #
-######################
+for m, metric in enumerate(["accuracy", "loss"]):
+    for xaxis in ["buffer", "params"]:
+        for wrt in ["width", "depth"]:
 
-plt.figure(figsize=(6, 6))
-plt.xlabel("added layer width (%)")
-plt.ylabel("accuracy barrier reduction (%)")
-plt.xticks(torch.linspace(0, 100, 11))
-plt.title("Accuracy barrier reduction w.r.t. added buffer\nCIFAR10, VGG11, bn=True")
+            plt.figure(figsize=(6, 6))
+            plt.xlabel("added layer width (%)" if wrt == "width" else "added parameters (%)")
+            plt.ylabel(f"{metric} barrier reduction (%)")
+            plt.xticks(torch.linspace(0, 100, 11))
+            plt.title(f"Accuracy barrier reduction w.r.t. added {wrt}\nCIFAR10, VGG11, bn=True")
 
-# AUC diagonal
-sns.lineplot(x=torch.linspace(0, 100, 11), y=torch.linspace(0, 100, 11), color="grey")
+            # AUC diagonal
+            sns.lineplot(x=torch.linspace(0, 100, 11), y=torch.linspace(0, 100, 11), color="grey")
 
-# 100% horizontal line
-sns.lineplot(x=torch.linspace(0, 100, 11), y=[100] * 11, color="grey")
+            # 100% horizontal line
+            sns.lineplot(x=torch.linspace(0, 100, 11), y=[100] * 11, color="grey")
 
-param_increase = (
-    torch.Tensor([1, 1.1897, 1.3587, 1.5107, 1.6398, 1.74, 1.8396, 1.9093, 1.960, 1.9899, 1.9999]) - 1
-) * 100
-# sns.lineplot(x=torch.linspace(0, 100, 11), y=param_increase, color="black", label="parameter increase (%)")
+            param_increase = (
+                torch.Tensor([1, 1.1897, 1.3587, 1.5107, 1.6398, 1.74, 1.8396, 1.9093, 1.960, 1.9899, 1.9999]) - 1
+            ) * 100
 
-for idx, (width, color) in enumerate(zip(widths, ["orangered", "orange", "mediumturquoise", "mediumvioletred"])):
-    sns.lineplot(
-        x=torch.linspace(0, 100, 11),
-        y=barrier_reduction_relative[idx][0] * 100,
-        label=width,
-        color=color,
-        marker="o",
-        markersize=4,
-    )
-    sns.lineplot(
-        x=torch.linspace(0, 100, 11),
-        y=barrier_reduction_relative_REPAIR[idx][0] * 100,
-        dashes=(2, 2),
-        color=color,
-        marker="o",
-        markersize=4,
-    )
+            for idx, (width, color) in enumerate(
+                zip(widths, ["orangered", "orange", "mediumturquoise", "mediumvioletred"])
+            ):
+                sns.lineplot(
+                    x=torch.linspace(0, 100, 11) if xaxis == "buffer" else param_increase,
+                    y=barrier_reduction_relative[m][idx][0] * 100
+                    if wrt == "width"
+                    else barrier_reduction_relative[m][2][idx] * 100,
+                    label=width,
+                    color=color,
+                    marker="o",
+                    markersize=4,
+                )
+                sns.lineplot(
+                    x=torch.linspace(0, 100, 11) if xaxis == "buffer" else param_increase,
+                    y=barrier_reduction_relative_REPAIR[m][idx][0] * 100
+                    if wrt == "width"
+                    else barrier_reduction_relative_REPAIR[m][2][idx] * 100,
+                    dashes=(2, 2),
+                    color=color,
+                    marker="o",
+                    markersize=4,
+                )
 
-# just for the REPAIR label
-sns.lineplot(x=[0, 0], y=[0, 0], dashes=(2, 2), label="with REPAIR", color="grey")
+            # just for the REPAIR label
+            sns.lineplot(x=[0, 0], y=[0, 0], dashes=(2, 2), label="with REPAIR", color="grey")
 
-plots_dir = get_plots_dir(subdir=Path(__file__).stem)
-plt.savefig(
-    os.path.join(
-        plots_dir,
-        "AUC_VGG11_buffer.png",
-    ),
-    dpi=600,
-)
-plt.close()
-print("📊 AUC VGG11 (w.r.t. buffer) plot saved")
-
-
-######################
-# plot w.r.t. params #
-######################
-
-plt.figure(figsize=(6, 6))
-plt.xlabel("added non-zero parameter count (%)")
-plt.ylabel("accuracy barrier reduction (%)")
-plt.xticks(torch.linspace(0, 100, 11))
-plt.title("Accuracy barrier reduction w.r.t. added parameters\nCIFAR10, VGG11, bn=True")
-
-
-# AUC diagonal
-sns.lineplot(x=torch.linspace(0, 100, 11), y=torch.linspace(0, 100, 11), color="grey")
-
-# 100% horizontal line
-sns.lineplot(x=torch.linspace(0, 100, 11), y=[100] * 11, color="grey")
-
-for idx, (width, color) in enumerate(zip(widths, ["orangered", "orange", "mediumturquoise", "mediumvioletred"])):
-    sns.lineplot(
-        x=param_increase, y=barrier_reduction_relative[idx][0] * 100, label=width, color=color, marker="o", markersize=4
-    )
-    sns.lineplot(
-        x=param_increase,
-        y=barrier_reduction_relative_REPAIR[idx][0] * 100,
-        dashes=(2, 2),
-        color=color,
-        marker="o",
-        markersize=4,
-    )
-
-# just for the REPAIR label
-sns.lineplot(x=[0, 0], y=[0, 0], dashes=(2, 2), label="with REPAIR", color="grey")
-
-plots_dir = get_plots_dir(subdir=Path(__file__).stem)
-plt.savefig(
-    os.path.join(
-        plots_dir,
-        "AUC_VGG11_params.png",
-    ),
-    dpi=600,
-)
-plt.close()
-print("📊 AUC VGG11 (w.r.t. parameters) plot saved")
-
-
-#####################
-# plot w.r.t. depth #
-#####################
-
-plt.figure(figsize=(6, 6))
-plt.xlabel("added layer width (%)")
-plt.ylabel("accuracy barrier reduction (%)")
-plt.xticks(torch.linspace(0, 100, 11))
-plt.title("Accuracy barrier reduction w.r.t. added buffer\nCIFAR10, width=1x, bn=True")
-
-# AUC diagonal
-sns.lineplot(x=torch.linspace(0, 100, 11), y=torch.linspace(0, 100, 11), color="grey")
-
-# 100% horizontal line
-sns.lineplot(x=torch.linspace(0, 100, 11), y=[100] * 11, color="grey")
-
-for idx, (size, color) in enumerate(zip(sizes, ["orangered", "orange", "mediumturquoise", "mediumvioletred"])):
-    sns.lineplot(
-        x=torch.linspace(0, 100, 11),
-        y=barrier_reduction_relative[2][idx] * 100,
-        label=f"VGG{size}",
-        color=color,
-        marker="o",
-        markersize=4,
-    )
-    sns.lineplot(
-        x=torch.linspace(0, 100, 11),
-        y=barrier_reduction_relative_REPAIR[2][idx] * 100,
-        dashes=(2, 2),
-        color=color,
-        marker="o",
-        markersize=4,
-    )
-
-# just for the REPAIR label
-sns.lineplot(x=[0, 0], y=[0, 0], dashes=(2, 2), label="with REPAIR", color="grey")
-
-plots_dir = get_plots_dir(subdir=Path(__file__).stem)
-plt.savefig(
-    os.path.join(
-        plots_dir,
-        "AUC_VGG_1x_sizes.png",
-    ),
-    dpi=600,
-)
-plt.close()
-print("📊 AUC VGG11 (w.r.t. sizes) plot saved")
+            plots_dir = get_plots_dir(subdir=Path(__file__).stem)
+            plt.savefig(
+                os.path.join(
+                    plots_dir,
+                    f"{metric}_AUC_VGG_{wrt}_{xaxis}.png",
+                ),
+                dpi=600,
+            )
+            plt.close()
+            print(f"📊 {metric} AUC VGG (w.r.t. {wrt} + {xaxis}) plot saved")
