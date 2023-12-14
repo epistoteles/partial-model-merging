0103c1acbbcae803d83d1be696c058b1c4f4634a
Korbinian Koch <korbinian-koch@web.de>
2023-12-14 23:08:16 +0100

    more plots for experiment c

--- src/plot_scripts/plot_AUC_experiment_c.py
+++ src/plot_scripts/plot_AUC_experiment_c.py
@@ -24,6 +24,18 @@ def get_metrics(dataset, architecture, size, bn, width):
         d["loss_ensembling"] = metrics["ensembling_test_losses"][10].item()
         d["loss_merging"] = metrics["merging_test_losses"][10].item()
         d["loss_merging_REPAIR"] = metrics["merging_REPAIR_test_losses"][10].item()
+        d["acc_expansions"] = torch.ones(11) * d["acc_merging"]
+        d["loss_expansions"] = torch.ones(11) * d["loss_merging"]
+        d["acc_expansions_REPAIR"] = torch.ones(11) * d["acc_merging_REPAIR"]
+        d["loss_expansions_REPAIR"] = torch.ones(11) * d["loss_merging_REPAIR"]
+        d["param_increase_expansions"] = (
+            torch.Tensor([1, 1.1897, 1.3587, 1.5107, 1.6398, 1.74, 1.8396, 1.9093, 1.960, 1.9899, 1.9999]) - 1
+        )
+        for idx, k in enumerate([1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0]):
+            d["acc_expansions"][idx + 1] = metrics[f"partial_merging_{k}_test_accs"][10]
+            d["loss_expansions"][idx + 1] = metrics[f"partial_merging_{k}_test_losses"][10]
+            d["acc_expansions_REPAIR"][idx + 1] = metrics[f"partial_merging_REPAIR_{k}_test_accs"][10]
+            d["loss_expansions_REPAIR"][idx + 1] = metrics[f"partial_merging_REPAIR_{k}_test_losses"][10]
     else:
         raise ValueError(f"File does not exist: {filename}")
     return d
@@ -39,7 +51,7 @@ width = 1
 for m, metric in enumerate(metrics):
     for dataset in datasets:
         dir = get_evaluations_dir(subdir="experiment_c")
-        filename = f"{dir}/experiment-c-{dataset}-{architecture}{size}-{'bn-' if bn else ''}{width}x-ab.safetensors"
+        filename = f"{dir}/OLD-experiment-c-{dataset}-{architecture}{size}-{'bn-' if bn else ''}{width}x-ab.safetensors"
         metrics = load_file(filename)
         metrics_dict = get_metrics(dataset, architecture, size, bn, width)
 
@@ -88,6 +100,24 @@ for m, metric in enumerate(metrics):
             y=torch.cat((first_value_REPAIR * 100, barrier_reduction_relative_REPAIR.T.flatten() * 100)),
             dashes=(2, 2),
             color="red",
+            # marker="o",
+            # markersize=4,
+        )
+        sns.lineplot(
+            x=metrics_dict["param_increase_expansions"] * 100,
+            y=(metrics_dict[f"{metric}_avg"] - metrics_dict[f"{metric}_expansions"]) / full_barrier_absolute * 100,
+            label="all layers",
+            color="orange",
+            # marker="o",
+            # markersize=4,
+        )
+        sns.lineplot(
+            x=metrics_dict["param_increase_expansions"] * 100,
+            y=(metrics_dict[f"{metric}_avg"] - metrics_dict[f"{metric}_expansions_REPAIR"])
+            / full_barrier_absolute
+            * 100,
+            dashes=(2, 2),
+            color="orange",
             # marker="o",
             # markersize=4,
         )
