bc77e4f57a9bf7c081d7f0aa362c8e2dea65520b
Korbinian Koch <korbinian-koch@web.de>
2023-12-18 21:10:20 +0100

    add adaptive plot

--- src/plot_scripts/plot_AUC_adaptive.py
+++ src/plot_scripts/plot_AUC_adaptive.py
@@ -18,7 +18,7 @@ metrics = ["acc", "loss"]
 sizes = [11] if architecture == "VGG" else [18]
 widths = [1] if architecture == "VGG" else [1, 2, 4, 8]
 expansions = [1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0]
-thresholds = [0, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.8, 1.0]
+thresholds = [0, 0.1, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.8, 1.0]
 
 endpoint = torch.zeros(len(metrics), len(widths), len(sizes))
 ensembling = torch.zeros(len(metrics), len(widths), len(sizes))
