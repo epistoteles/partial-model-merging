cfc83ed48a023c69e3646081100de04450d054eb
Korbinian Koch <korbinian-koch@web.de>
2023-12-20 16:15:06 +0100

    update adaptive plot

---
+++ src/plot_scripts/plot_adaptive_expansion.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import torch
+from pathlib import Path
+from safetensors.torch import load_file
+
+from src.utils import get_plots_dir, get_evaluations_dir
+import os
+
+
+dataset = "SVHN"
+architecture = "VGG"
+size = 11
+width = 1
+
+metrics = load_file(
+    os.path.join(
+        get_evaluations_dir(subdir="adaptive"), f"adaptive-{dataset}-{architecture}{size}-bn-{width}x-ab.safetensors"
+    )
+)
+
+
+thresholds = [0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.8, 1.0]
+
+plt.figure(figsize=(12, 8))
+plt.xlabel("layer")
+plt.ylabel("width increase")
+plt.title(f"expansions resulting from adaptive buffer assignment\n{dataset}, {architecture}{size}, {width}×width")
+
+for threshold in thresholds:
+    used_neurons_relative = metrics[f"adaptive_merging_{threshold:g}_used_neurons_relative"]
+    sns.lineplot(
+        x=range(1, len(used_neurons_relative) + 1),
+        y=used_neurons_relative,
+        label=f"epsilon={threshold:g}",
+        color=plt.cm.rainbow(threshold),
+    )
+
+plt.tight_layout()
+plots_dir = get_plots_dir(subdir=Path(__file__).stem)
+plt.savefig(
+    os.path.join(
+        plots_dir,
+        f"adaptive_expansion_{dataset}_{architecture}.png",
+    ),
+    dpi=600,
+    bbox_inches="tight",
+)
+plt.close()
+print(f"📊 adaptive expansion{dataset} {architecture}{size} plot saved")
