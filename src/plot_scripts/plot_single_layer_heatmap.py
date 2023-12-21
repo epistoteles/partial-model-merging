48c0e11571d401931c7c417f9769d8cca422e904
Korbinian Koch <korbinian-koch@web.de>
2023-12-21 01:34:07 +0100

    update random plot

--- src/plot_scripts/plot_single_layer_heatmap.py
+++ src/plot_scripts/plot_single_layer_heatmap.py
@@ -9,43 +9,51 @@ import os
 
 dataset = "SVHN"
 architecture = "VGG"
+size = 11
 bn = True
+width = 1
 
 metrics = ["acc", "loss"]
 
-for metric in metrics:
 
-    tensor1 = torch.randn(10, 8) * 10
-    tensor2 = torch.randn(10, 8) * 50
-    tensor3 = torch.randn(10, 8) * 100
+tensor1 = torch.randn(10, 8) * 10
+tensor2 = torch.randn(10, 8) * 50
+tensor3 = torch.randn(10, 8) * 100
 
-    # Creating subplots
-    fig, axes = plt.subplots(1, 3, figsize=(12, 5))
+# Creating subplots
+fig, axes = plt.subplots(1, 3, figsize=(12, 5))
 
-    # Plotting the heatmaps
-    sns.heatmap(tensor1, ax=axes[0], cbar=False)
-    sns.heatmap(tensor2, ax=axes[1], cbar=False)
-    sns.heatmap(tensor3, ax=axes[2], cbar=False)
+plt.title(f"Expanding individual layers\n{architecture}{size}, {dataset}, {width}×width")
 
-    # Setting up the axes
-    # Show y-axis only on the left subplot
-    axes[0].set_ylabel("gamma")
-    for ax in axes[1:]:
-        ax.set_ylabel("")
+axes[0].set_title("Test accuracy")
+axes[1].set_title("Test loss")
+axes[2].set_title("Parameter increase")
 
-    # Show x-axis on all subplots
-    for ax in axes:
-        ax.set_xlabel("layer")
+# Plotting the heatmaps
+sns.heatmap(tensor1, ax=axes[0], cbar=False)
+sns.heatmap(tensor2, ax=axes[1], cbar=False)
+sns.heatmap(tensor3, ax=axes[2], cbar=False)
 
-    plt.tight_layout()
-    plots_dir = "/home/korbinian/Documents/Masterarbeit/partial-model-merging/plots/plot_single_layer_heatmap/"
-    # plots_dir = get_plots_dir(subdir=Path(__file__).stem)
-    plt.savefig(
-        os.path.join(
-            plots_dir,
-            f"per_layer_{metric}_{dataset}_{architecture}.png",
-        ),
-        dpi=600,
-    )
-    plt.close()
-    print(f"📊 per layer {metric} {dataset} {architecture} plot saved")
+# Setting up the axes
+# Show y-axis only on the left subplot
+axes[0].set_ylabel("gamma")
+axes[0].set_yticklabels(reversed([f"{x:.1f}" for x in torch.linspace(0.1, 1, 10)]))
+for ax in axes[1:]:
+    ax.set_ylabel("")
+
+# Show x-axis on all subplots
+for ax in axes:
+    ax.set_xlabel("layer")
+
+plt.tight_layout()
+plots_dir = "/home/korbinian/Documents/Masterarbeit/partial-model-merging/plots/plot_single_layer_heatmap/"
+# plots_dir = get_plots_dir(subdir=Path(__file__).stem)
+plt.savefig(
+    os.path.join(
+        plots_dir,
+        f"per_layer_{dataset}_{architecture}.png",
+    ),
+    dpi=600,
+)
+plt.close()
+print(f"📊 per layer {dataset} {architecture} plot saved")
