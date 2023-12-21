e28492fc66a986e7acada524a414c0b128453a48
Korbinian Koch <korbinian-koch@web.de>
2023-12-21 01:47:19 +0100

    update random plot

--- src/plot_scripts/plot_AUC_adaptive.py
+++ src/plot_scripts/plot_AUC_adaptive.py
@@ -15,8 +15,8 @@ architecture = "VGG"
 bn = True
 
 metrics = ["acc", "loss"]
-sizes = [19] if architecture == "VGG" else [6]
-widths = [4] if architecture == "VGG" else [1]
+sizes = [11] if architecture == "VGG" else [18]
+widths = [1] if architecture == "VGG" else [1]
 expansions = [1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0]
 thresholds = [0, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.8]
 
